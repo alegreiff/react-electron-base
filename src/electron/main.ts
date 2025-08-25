53567cc760615531dd19eceab876625fe374f637
Jaime de Greiff <alegreiff@gmail.com>
2025-08-25 12:18:28 -0500

    package.json es ahora compatible windows / macos

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -7,9 +7,11 @@ app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
 
   if (isDev()) {
+    // SOLO EN DEV CARGA DESDE EL PROYECTO REACT
     mainWindow.loadURL('http://localhost:5123')
 
   } else {
+    // EN PROD CARGA EL PROYECTO FINAL
     mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react/index.html'))
   }
 
